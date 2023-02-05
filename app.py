fe269dbbe661c55476bd7d5df707053ea4be30b5
Mo Hobson <mohobson8@gmail.com>
2023-02-05 18:31:25 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        temperature = float(request.form["temperature"])
+        prompt = request.form["prompt"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(prompt),
+            temperature=temperature,
+            max_tokens=2040,
+            # stream=True,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(prompt):
+    return """ 
+    {}
+    """.format(
+        prompt.capitalize()
+    )
