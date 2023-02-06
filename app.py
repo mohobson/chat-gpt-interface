a9d80d8deb428ec3b6333f9925fa22a1c4edea3b
Mo Hobson <mohobson8@gmail.com>
2023-02-05 21:05:16 -0500

    added function around app

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 import os
-import time
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def create_app():
+    app = Flask(__name__)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        temperature = float(request.form["temperature"])
-        prompt = request.form["prompt"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(prompt),
-            temperature=temperature,
-            max_tokens=2040,
-            # stream=True,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+    @app.route("/", methods=("GET", "POST"))
+    def index():
+        if request.method == "POST":
+            temperature = float(request.form["temperature"])
+            prompt = request.form["prompt"]
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=generate_prompt(prompt),
+                temperature=temperature,
+                max_tokens=2040,
+                # stream=True,
+            )
+            return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        result = request.args.get("result")
+        return render_template("index.html", result=result)
+    
+    return app
 
 
 def generate_prompt(prompt):
@@ -32,3 +34,5 @@ def generate_prompt(prompt):
     """.format(
         prompt.capitalize()
     )
+
+app = create_app()
